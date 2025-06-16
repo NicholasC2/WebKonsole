9f5ac0ada060ee645cd111b058149cfb9f67d044
NicholasC2 <137032388+NicholasC2@users.noreply.github.com>
2025-06-17 11:58:55 +1200

    Update files

--- Konsole.js
+++ Konsole.js
@@ -1,1 +1,84 @@
+class Konsole {
+    constructor(container, options = {
+        width: "100%",
+        height: "100%",
+        textColor: "lime",
+        backgroundColor: "black",
+        font: "monospace",
+        initCommand: "echo {version}",
+        prefix: "$",
+        commands: [
+            {
+                "alias": ["echo", "print"],
+                "run": (alias, args)=>{
+                    this.buffer.push(args.join(" "))
+                }
+            }
+        ],
+        variables: {
+            "version": "Konsole v1.0.0"
+        }
+    }) {
+        if(!container instanceof HTMLElement) {
+            throw new Error("Container must be an Element")
+        }
+        this.container = container
+        this.options = options
+        this.buffer = []
+        this.prefix = options.prefix
+        this.commands = commands
+        this.variables = this.variables
+        Object.assign(this.container.style, {
+            backgroundColor: backgroundColor,
+            color: textColor,
+            fontFamily: font,
+            width: width,
+            height: height,
+            overflowY: "scroll",
+            whiteSpace: "pre-wrap",
+            padding: "5px",
+            boxSizing: "border-box"
+        })
+        this.container.addEventListener("keydown", (event) => {
+            if(event.key == "Enter") {
+                const inputText = this.buffer[this.buffer.length-1].slice(1,0)
+                this.buffer.push(this.prefix+inputText)
+                this.runCommand(inputText)
+            }
+            this.buffer[this.buffer.length-1] += event.key
+            this.update()
+        })
+        this.runCommand(options.initCommand)
+        this.update()
+    }
 
+    update() {
+        this.container.innerText = this.buffer.join("\n")
+    }
+
+    replaceVars(text = "") {
+        const variables = Object.entries(this.variables);
+        variables.forEach((variable) => {
+            const [key, value] = variable
+            text = text.replaceAll(`{${key}}`, value)
+        })
+        return text
+    }
+
+    runCommand(cmd = "") {
+        cmd = this.replaceVars(cmd)
+        const args = cmd.split(" ")
+        const alias = args.shift()
+        let found = false
+        for (const command of this.commands) {
+            if(command.alias.includes(alias)) {
+                command.run(alias, args)
+                found = true
+                break;
+            }
+        }
+        if(!found) {
+            this.buffer.push(`Unknown command: ${alias}`)
+        }
+    }
+}
