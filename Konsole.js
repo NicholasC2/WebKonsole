ddc8931a87dd3545bb3785838b2b674a44a0176c
NicholasC2 <niccartyr@gmail.com>
2025-06-18 10:32:57 +1200

    update

--- Konsole.js
+++ Konsole.js
@@ -10,15 +10,13 @@ class Konsole {
         commands: [
             {
                 "alias": ["echo", "print"],
-                "run": (alias, args)=>{
-                    return new Promise((resolve)=>{
-                        resolve(args.join(" "))
-                    })
+                "run": async(alias, args)=>{
+                    return Promise.resolve(args.join(" "));
                 }
             },
             {
                 "alias": ["clear", "cls"],
-                "run": ()=>{
+                "run": async()=>{
                     return new Promise((resolve)=>{
                         this.buffer = [this.options.prefix]
                         resolve()
@@ -27,10 +25,16 @@ class Konsole {
             },
             {
                 "alias": ["wait"],
-                "run": (alias, args)=>{
+                "run": async(alias, args)=>{
                     return new Promise((resolve)=>{
                         setTimeout(resolve, args[0])
                     })
+                }
+            },
+            {
+                "alias": ["yoshikage"],
+                "run": async(alias, args)=>{
+                    return Promise.resolve("My name is Yoshikage Kira. I'm 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married. I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest. I don't smoke, but I occasionally drink. I'm in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue or stress in the morning. I was told there were no issues at my last check-up. I'm trying to explain that I'm a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn't lose to anyone.")
                 }
             }
         ],
@@ -44,7 +48,9 @@ class Konsole {
         this.container = container
         this.options = options
         this.buffer = []
-        this.update()
+        setInterval(()=>{
+            this.update()
+        }, 16)
         window.addEventListener("keydown", async(event) => {
             event.preventDefault()
             if(event.key == "Enter") {
@@ -74,9 +80,7 @@ class Konsole {
             }
             this.update()
         })
-        this.runCommand(options.initCommand).then(()=>{
-            this.update()
-        })
+        this.runCommand(options.initCommand)
     }
 
     update() {
@@ -105,35 +109,37 @@ class Konsole {
         })
     }
 
-    async runCommand(text = "") {
-        return new Promise(async(resolve)=>{
-            for(var cmd of text.split("\n")) {
-                if(cmd.trim() == "") {
-                    this.buffer.push("")
-                    continue
-                }
-                cmd = await this.replaceVars(cmd)
-                const args = cmd.split(" ")
-                const alias = args.shift()
-                let found = false
-                for (const command of this.options.commands) {
-                    if(command.alias.includes(alias)) {
+    async runCommand(text) {
+        for (var cmd of text.split("\n")) {
+            if (cmd.trim() === "") {
+                this.buffer.push("");
+                continue;
+            }
+
+            cmd = await this.replaceVars(cmd);
+            const args = cmd.split(" ");
+            const alias = args.shift();
+            let found = false;
+
+            for (const command of this.options.commands) {
+                if (command.alias.includes(alias)) {
+                    this.buffer.push("");
+                    const output = await command.run(alias, args);
+                    if (output) {
+                        this.buffer[this.buffer.length - 1] = output;
                         this.buffer.push("")
-                        this.update()
-                        const output = await command.run(alias, args)
-                        if(output) {
-                            this.buffer[this.buffer.length-1] = output
-                        }
-                        found = true
-                        break;
                     }
-                }
-                if(!found) {
-                    this.buffer.push(`Unknown command: ${alias}`)
+                    found = true;
+                    break;
                 }
             }
-            this.buffer[this.buffer.length-1] = this.options.prefix
-            resolve()
-        })
+
+            if (!found) {
+                this.buffer.push(`Unknown command: ${alias}`);
+            }
+        }
+
+        this.buffer[this.buffer.length - 1] = this.options.prefix;
+        return;
     }
 }
