9e6bff4680f63fe2f34422341cd40daed309e046
John Doe <john.doe@example.com>
2025-06-18 09:54:00 +1200

    update

--- Konsole.js
+++ Konsole.js
@@ -11,8 +11,26 @@ class Konsole {
             {
                 "alias": ["echo", "print"],
                 "run": (alias, args)=>{
-                    this.buffer.push(args.join(" "))
-                    return
+                    return new Promise((resolve)=>{
+                        resolve(args.join(" "))
+                    })
+                }
+            },
+            {
+                "alias": ["clear", "cls"],
+                "run": ()=>{
+                    return new Promise((resolve)=>{
+                        this.buffer = [this.options.prefix]
+                        resolve()
+                    })
+                }
+            },
+            {
+                "alias": ["wait"],
+                "run": (alias, args)=>{
+                    return new Promise((resolve)=>{
+                        setTimeout(resolve, args[0])
+                    })
                 }
             }
         ],
@@ -26,65 +44,96 @@ class Konsole {
         this.container = container
         this.options = options
         this.buffer = []
-        Object.assign(this.container.style, {
-            backgroundColor: options.backgroundColor,
-            color: options.textColor,
-            fontFamily: options.font,
-            width: options.width,
-            height: options.height,
-            overflowY: "scroll",
-            whiteSpace: "pre-wrap",
-            padding: "5px",
-            boxSizing: "border-box"
-        })
-        window.addEventListener("keydown", (event) => {
+        this.update()
+        window.addEventListener("keydown", async(event) => {
+            event.preventDefault()
             if(event.key == "Enter") {
-                const inputText = this.buffer[this.buffer.length-1].slice(1,this.buffer[this.buffer.length-1].length)
-                this.buffer.push(options.prefix+inputText)
-                if(inputText.trim() != "") {
-                    this.runCommand(inputText)
+                if(event.ctrlKey) {
+                    this.buffer[this.buffer.length-1] += "\n"
+                } else {
+                    const current_buffer = this.buffer[this.buffer.length-1]
+                    const inputText = current_buffer.slice(this.options.prefix.length,current_buffer.length)
+                    await this.runCommand(inputText)
                 }
             }
-            if(event.key.length == 1) {
+            if(event.key == "Backspace") {
+                if(event.shiftKey) {
+                    this.buffer[this.buffer.length-1] = this.options.prefix
+                } else {
+                    const current_buffer = this.buffer[this.buffer.length-1]
+                    if(current_buffer.slice(0,current_buffer.length - 1).startsWith(this.options.prefix)) {
+                        this.buffer[this.buffer.length-1] = current_buffer.slice(0,current_buffer.length - 1)
+                    }
+                }
+            }
+            if(event.key == "l" && event.ctrlKey && !event.altKey && !event.shiftKey) {
+                this.buffer = [this.options.prefix]
+            }
+            if(event.key.length == 1 && !event.ctrlKey && !event.altKey) {
                 this.buffer[this.buffer.length-1] += event.key
             }
             this.update()
         })
-        this.runCommand(options.initCommand)
-        this.update()
+        this.runCommand(options.initCommand).then(()=>{
+            this.update()
+        })
     }
 
     update() {
-        this.container.innerText = this.buffer.join("\n")
-    }
-
-    replaceVars(text = "") {
-        const variables = Object.entries(this.options.variables);
-        variables.forEach((variable) => {
-            const [key, value] = variable
-            text = text.replaceAll(`{${key}}`, value)
+        Object.assign(this.container.style, {
+            backgroundColor: this.options.backgroundColor,
+            color: this.options.textColor,
+            fontFamily: this.options.font,
+            width: this.options.width,
+            height: this.options.height,
+            overflowY: "auto",
+            whiteSpace: "pre-wrap",
+            padding: "5px",
+            boxSizing: "border-box"
         })
-        return text
+        this.container.innerText = this.buffer.join("\n")+"|"
     }
 
-    runCommand(cmd = "") {
-        cmd = this.replaceVars(cmd)
-        const args = cmd.split(" ")
-        const alias = args.shift()
-        let found = false
-        for (const command of this.options.commands) {
-            if(command.alias.includes(alias)) {
-                const output = command.run(alias, args)
-                if(output) {
-                    this.buffer.push(output)
+    async replaceVars(text = "") {
+        return new Promise(async(resolve)=>{
+            const variables = Object.entries(this.options.variables);
+            variables.forEach((variable) => {
+                const [key, value] = variable
+                text = text.replaceAll(`{${key}}`, value)
+            })
+            resolve(text)
+        })
+    }
+
+    async runCommand(text = "") {
+        return new Promise(async(resolve)=>{
+            for(var cmd of text.split("\n")) {
+                if(cmd.trim() == "") {
+                    this.buffer.push("")
+                    continue
                 }
-                found = true
-                break;
+                cmd = await this.replaceVars(cmd)
+                const args = cmd.split(" ")
+                const alias = args.shift()
+                let found = false
+                for (const command of this.options.commands) {
+                    if(command.alias.includes(alias)) {
+                        this.buffer.push("")
+                        this.update()
+                        const output = await command.run(alias, args)
+                        if(output) {
+                            this.buffer[this.buffer.length-1] = output
+                        }
+                        found = true
+                        break;
+                    }
+                }
+                if(!found) {
+                    this.buffer.push(`Unknown command: ${alias}`)
+                }
             }
-        }
-        if(!found) {
-            this.buffer.push(`Unknown command: ${alias}`)
-        }
-        this.buffer.push(this.options.prefix)
+            this.buffer[this.buffer.length-1] = this.options.prefix
+            resolve()
+        })
     }
 }
