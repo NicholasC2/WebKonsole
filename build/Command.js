200b2528198bca6202b643f64711a65ce551612a
Boxy <nozakisola@gmail.com>
2025-06-28 00:26:35 -0500

    Remove Jeff

--- build/Command.js
+++
@@ -1,9 +0,0 @@
-export default class Command {
-    constructor(alias = [], description = "", run) {
-        this.alias = alias;
-        this.description = description;
-        this.run = run;
-    }
-}
-;
-//# sourceMappingURL=Command.js.map
--- build/frontend.js
+++
@@ -1,6 +0,0 @@
-import Konsole from "./Konsole.js";
-import HTTP from "./HTTP.js";
-const container = document.querySelector(".konsole");
-const konsole = new Konsole(container);
-konsole.register(new HTTP);
-//# sourceMappingURL=frontend.js.map
